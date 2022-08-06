5419b374862e21616c114737f63d85424c8528cc
Dylan Dunne <dylanadunne@gmail.com>
2022-08-05 23:51:39 -0400

    added all calculation funcs

--- calc-app/src/app/app.component.ts
+++ calc-app/src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc-app';
+  firstOperand = '';
+  secondOperand = '';
+  operator = '';
+  calculation = 0;
+
+
+  insertNumber(num: string) {
+    if (this.firstOperand && this.operator) {
+      this.secondOperand = this.secondOperand + num
+    } else {
+      this.firstOperand = this.firstOperand + num;
+    }
+  }
+
+  insertOperator(op: string) {
+    if (this.firstOperand) {
+      this.operator = op;
+    }
+  }
+
+  runCalculation() {
+    switch (this.operator) {
+      case '+':
+        this.calculation = Number(this.firstOperand) + Number(this.secondOperand)
+        break;
+      case '-':
+        this.calculation = Number(this.firstOperand) - Number(this.secondOperand)
+        break;
+      case '*':
+        this.calculation = Number(this.firstOperand) * Number(this.secondOperand)
+        break;
+      case '÷':
+        this.calculation = Number(this.firstOperand) / Number(this.secondOperand)
+        break;
+      default:
+        'error'
+    }
+  }
+
+  allClear() {
+    this.firstOperand = '';
+    this.secondOperand = '';
+    this.operator = '';
+    this.calculation= 0;
+  }
 }
